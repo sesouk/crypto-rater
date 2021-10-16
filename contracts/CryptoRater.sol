a9c9ca3d5f0c2e3e8ffc185a70c51c7eb5ec9906
Kevin Tran <sesouk@gmail.com>
2021-10-16 13:07:34 -0600

    store data

--- contracts/CryptoRater.sol
+++ contracts/CryptoRater.sol
@@ -5,7 +5,30 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract CryptoRater {
+  uint256 plusTotal;
+  uint256 minusTotal;
+
   constructor() {
     console.log("Summoning justsu! Smart contract!");
+  }
+
+  function plus() public {
+    plusTotal += 1;
+    console.log("%s rated positively!", msg.sender);
+  }
+
+  function minus() public {
+    minusTotal += 1;
+    console.log("%s rated negatively!", msg.sender);
+  }
+
+  function getPlus() public view returns (uint256) {
+    console.log("Positive rating of %d :)", plusTotal);
+    return plusTotal;
+  }
+
+  function getMinus() public view returns (uint256) {
+    console.log("Negative rating of %d :(", plusTotal);
+    return minusTotal;
   }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,31 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const rateContractFactory = await hre.ethers.getContractFactory("CryptoRater");
   const rateContract = await rateContractFactory.deploy();
   await rateContract.deployed();
+
   console.log("Contract summoned at:", rateContract.address);
+  console.log("Contract summoned by:", owner.address);
+
+  let plusCount;
+  plusCount = await rateContract.getPlus()
+
+  let minusCount;
+  minusCount = await rateContract.getMinus()
+
+  let plusTxn = await rateContract.plus()
+  await plusTxn.wait()
+
+  let minusTxn = await rateContract.minus()
+  await minusTxn.wait()
+
+  plusCount = await rateContract.getPlus()
+  minusCount = await rateContract.getMinus()
+
+  plusTxn = await rateContract.connect(randomPerson).plus()
+  await plusTxn.wait()
+
+  plusCount = await rateContract.getPlus()
 }
 
 const runMain = async () => {
